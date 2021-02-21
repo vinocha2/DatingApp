33cd7bcafa9a304ed54ef6951b4095f33764bd65
Anant <vinocha@gmail.com>
2021-02-21 19:09:08 +0530

    Section 4

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -5,11 +5,13 @@ import {HttpClientModule} from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      ValueComponent,
+      NavComponent
    ],
    imports: [
       BrowserModule,
