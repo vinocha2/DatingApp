b62940af05bf454916fb130fa09edfb6139a8a80
Anant <vinocha@gmail.com>
2021-03-25 12:39:52 +0530

    Added Postgres

--- DatingApp-SPA/src/app/_interceptors/loading.interceptor.ts
+++ DatingApp-SPA/src/app/_interceptors/loading.interceptor.ts
@@ -17,7 +17,7 @@ export class LoadingInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     this.busyService.busy();
     return next.handle(request).pipe(
-      delay(2000),
+      //delay(2000),
       finalize(() => {
         console.log("loading");
         this.busyService.idle();
